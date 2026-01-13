58984c95a1fa3b82adbed804a5712e779067b615
gernotschulmeister <gernotschulmeister@gmx.at>
2026-01-13 18:49:27 +0100

    update definition.completion

--- src/main/java/definition/completion/original/EmailSender.java
+++ src/main/java/definition/completion/original/EmailSender.java
@@ -1,7 +1,10 @@
 package definition.completion.original;
 
+import static original.EmailSender.EXTERNAL_SERVICE_NOT_IMPLEMENTED;
+
 public class EmailSender {
     public String send(String message) {
-        throw new UnsupportedOperationException("external service not implemented");
+
+        throw new UnsupportedOperationException(EXTERNAL_SERVICE_NOT_IMPLEMENTED);
     }
 }
